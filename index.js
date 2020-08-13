4af5ab8a3852248e8b781e548e17f10a4e6ea346
Josh Dunson <jdunson98@yahoo.com>
2020-08-13 15:03:55 -0500

    should be deployed

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // code away!
+require('cross-env').config();
 const server = require('./server');
 
-const port = process.send.PORT || 4000
+const port = process.send.PORT || 4000;
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`)
